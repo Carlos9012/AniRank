ef51860d80d3a1110faafdc3c906d2a6f4f46292
Francisco Oliveira <francisco.carlos1564@gmail.com>
2026-08-13 10:53:16 -0300

    fix: exporta todos os routers no __init__

--- Back/app/routes/__init__.py
+++ Back/app/routes/__init__.py
@@ -1,1 +1,13 @@
-﻿
+﻿from .animes import router as animes_router
+from .auth import router as auth_router
+from .user_list import router as list_router
+from .recommendations import router as recommendations
+from .home import router as home_router
+
+__all__ = [
+    'animes_router',
+    'auth_router',
+    'list_router',
+    'recommendations',
+    'home_router',
+]
